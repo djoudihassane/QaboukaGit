82dbd66e4c68234a08bcdc332c72a8c4dd22a4aa
boukandi <59587948+boukandi@users.noreply.github.com>
2020-02-11 20:08:23 -0600

    first commit

---
+++ NewJava/src/QAaccount/GitHubClass.java
@@ -0,0 +1,11 @@
+package QAaccount;
+
+public class GitHubClass {
+	public void print() {
+		System.out.println("setting up gethub platform");
+	}
+	
+public void secondCommit() {
+	System.out.println("hello word");
+}
+}
